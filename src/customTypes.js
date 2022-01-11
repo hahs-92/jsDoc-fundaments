63e67487ef95126db9c363d4affadb790b485d0f
Alex <alexandre_2420@outlook.com>
2022-01-11 09:45:33 -0500

    jsDoc with functions and customTypes

---
+++ src/customTypes.js
@@ -0,0 +1,19 @@
+/**
+ * User
+ * @typedef{Object} User
+ * @property {Number} id
+ * @property {string} name
+ * @property {Number} [age] optional
+ * @property {boolean} isActive
+ */
+
+
+/**
+ * @type{User}
+ */
+const user1 = {
+    id: 1,
+    name: "Alex",
+    age: 29,
+    isActive: true
+}
---
+++ src/functions.js
@@ -0,0 +1,17 @@
+/**
+ * @description Add to numbers
+ * @param {Number} a First param
+ * @param {Number} b Second param
+ * @returns {Number} total
+ */
+function add(a, b) {
+    return a + b
+}
+
+/**
+ *
+ * @param {Number} a
+ * @param {Number} b
+ * @returns string
+ */
+const addTwo = (a, b) => `The result is: ${a + b}`
