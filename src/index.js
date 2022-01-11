d78b87dbad01edf2385b9c4a94d814272f680eea
Alex <alexandre_2420@outlook.com>
2022-01-11 08:29:30 -0500

    add ts-check config and objects with docs

---
+++ src/index.js
@@ -0,0 +1,20 @@
+
+/**
+ * This users'name
+ * @type{string}
+ * */
+let fullName = "Alex Hernandez"
+
+
+/**
+ * List of the users's age
+ * @type{Array<Number>}
+ */
+const ages = [13, 45, 65, 67]
+
+
+/**
+ * This is an array of number and strings
+ * @type{Array<Number | string>}
+ */
+const scoreStrings = [10, "12", 33]
---
+++ src/objects.js
@@ -0,0 +1,25 @@
+/**
+ * Person Object
+ * @type{{id: Number, firstName: string, lastName: string, age: Number}}
+ */
+const person = {
+    id: 1,
+    firstName: "Alex",
+    lastName: "Hernandez",
+    age: 29
+}
+
+
+/**
+ * Person Object 2
+ * @type{{id: number | string, firstName: string, lastName: string, age: string | Number}}
+ */
+const person2 = {
+    id: 1,
+    firstName: "Alex",
+    lastName: "Hernandez",
+    age: 29
+}
+
+
+
